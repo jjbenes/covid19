63d8116972380fb2e264ce1a5fe80c6c023a530b
jj benes <ephrem>
2020-05-07 18:55:02 -0700

    Added dictionary for 10 largest U.S. metro areas

--- python/fp_covid19/visualization/geojson_helper.py
+++ python/fp_covid19/visualization/geojson_helper.py
@@ -10,9 +10,103 @@ STATES_JSON = ('https://raw.githubusercontent.com/jjbenes/covid19/master/json/'
                'us-states.json')
 COUNTIES_JSON = ('https://first-principles.ai/geojson/us-counties.json')
 
+US_ATLANTA_METRO_FIPS = [
+    '13013', '13015', '13035', '13045', '13057', '13063', '13067', '13077',
+    '13085', '13089', '13097', '13113', '13117', '13121', '13135', '13139',
+    '13143', '13149', '13151', '13159', '13171', '13199', '13211', '13217',
+    '13223', '13227', '13231', '13247', '13255', '13297']
+"""U.S. Atlanta Metropolitan Area: [
+   'Barrow County, GA', 'Bartow County, GA' , 'Butts County, GA',
+   'Carroll County, GA', 'Cherokee County, GA', 'Clayton County, GA',
+   'Cobb County, GA', 'Coweta County, GA', 'Dawson County, GA',
+   'DeKalb County, GA', 'Douglas County, GA', 'Fayette County, GA',
+   'Forsyth County, GA', 'Fulton County, GA', 'Gwinnett County, GA',
+   'Hall County, GA', 'Haralson County, GA', 'Heard County, GA',
+   'Henry County, GA', 'Jasper County, GA', 'Lamar County, GA',
+   'Meriwether County, GA', 'Morgan County, GA', 'Newton County, GA',
+   'Paulding County, GA', 'Pickens County, GA', 'Pike County, GA',
+   'Rockdale County, GA', 'Spalding County, GA', 'Walton County, GA']
+"""
+
+US_CHICAGO_METRO_FIPS = [
+    '17031', '17037', '17043', '17063', '17089', '17091', '17093', '17097',
+    '17111', '17197', '18073', '18089', '18111', '18127', '55059']
+""" U.S. Chicago Metropolitan Area:[
+    'Cook County, IL', 'DeKalb County, IL', 'DuPage County, IL',
+    'Grundy County, IL', 'Kankakee County, IL', 'Kane County, IL',
+    'Kendall County, IL', 'McHenry County, IL', 'Will County, IL',
+    'Jasper County, IN', 'Lake County, IN', 'Newton County, IN',
+    'Porter County, IN', 'Lake County, IL', 'Kenosha County, WI']
+"""
+
+US_DALLAS_FORT_WORTH_METRO_FIPS = [
+    '48085', '48113', '48121', '48139', '48231', '48251', '48257', '48367',
+    '48397', '48439', '48497']
+""" U.S. Dallas-Fort-Worth Metroplex: [
+   'Collin County, TX', 'Dallas County, TX', 'Denton County, TX',
+   'Ellis County, TX', 'Hunt County, TX', 'Kaufman County, TX',
+   'Rockwall County, TX', 'Johnson County, TX', 'Parker County, TX',
+   'Tarrant County, TX', 'Wise County, TX']
+"""
+
+US_GREATER_BOSTON_FIPS = [
+    '25009', '25017', '25021', '25023', '25025', '33015', '33017']
+"""U.S. Greater Boston: [
+   'Norfolk County, MA', 'Plymouth County, MA', 'Suffolk County, MA',
+   'Essex County, MA', 'Middlesex County, MA', 'Rockingham County, NH',
+   'Strafford County, NH']
+"""
+
+US_GREATER_HOUSTON_FIPS = [
+    '48015', '48039', '48071', '48157', '48167', '48201', '48291', '48339',
+    '48473']
+"""U.S. Greater Houston: [
+   'Harris County, TX', 'Fort Bend County, TX', 'Montgomery County, TX',
+   'Brazoria County, TX', 'Galveston County, TX', 'Liberty County, TX',
+   'Waller County, TX', 'Chambers County, TX', 'Austin County, TX']
+"""
+
+US_GREATER_LA_FIPS = ['6037', '6059', '6065', '6071', '6111']
+"""U.S. Greater Los Angeles: Los Angeles, Orange, Riverside, San Bernadino,
+   Ventura
+"""
+
+US_GREATER_PHILADELPHIA_FIPS = [
+    '10003', '24015', '34005', '34007', '34015', '34033', '42017', '42029',
+    '42045', '42091', '42101']
+"""U.S. Greater Philadelphia: [
+   'Burlington County, NJ', 'Camden County, NJ', 'Gloucester County, NJ',
+   'Bucks County, PA', 'Chester County, PA', 'Montgomery County, PA',
+   'Delaware County, PA', 'Philadelphia County, PA',
+   'New Castle County, DE', 'Cecil County, MD', 'Salem County, NJ']
+"""
+
+US_MIAMI_METRO_FIPS = ['12011', '12086', '12099']
+"""U.S. Miami Metropolitan Area: [
+   'Miami-Dade County, FL', 'Broward County, FL', 'Palm Beach County, FL']
+"""
+
+US_SF_BAY_AREA_FIPS = ['6001', '6013', '6041', '6055', '6075',
+                       '6081', '6085', '6095', '6097']
+"""U.S. San Francisco Bay Area: Alameda, Contra Costa, Marin, Napa,
+   San Francisco, San Mateo, Santa Clara, Solano, and Sonoma"""
+
 US_NYC_FIPS = ['36005', '36047', '36061', '36081', '36085']
 """U.S. New York City FIPS: Bronx, Kings (Brooklyn), New York (Manhattan),
    Queens, and Richmond (Staten Island)"""
+
+US_METROS = {
+    'Atlanta Metropolian Area, GA': US_ATLANTA_METRO_FIPS,
+    'Chicago Metropolitan Area, IL, IN, WI': US_CHICAGO_METRO_FIPS,
+    'Dallas-Fort Worth Metroplex, TX': US_DALLAS_FORT_WORTH_METRO_FIPS,
+    'Greater Boston, MA, NH': US_GREATER_BOSTON_FIPS,
+    'Greater Houston, TX': US_GREATER_HOUSTON_FIPS,
+    'Greater Los Angeles, CA': US_GREATER_LA_FIPS,
+    'Greater Philadelphia, DE, NJ, PA': US_GREATER_PHILADELPHIA_FIPS,
+    'Miami Metropolitan Area, FL': US_MIAMI_METRO_FIPS,
+    'New York City, NY': US_NYC_FIPS,
+    'San Francisco Bay Area, CA': US_SF_BAY_AREA_FIPS,
+}
 
 USPS_PUB28_DF = pd.DataFrame(
     [
